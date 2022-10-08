a0851866348c29bbafb8cf16a5aa398bea11f111
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-10-08 14:46:58 +0200

    GL01 - add a note

--- lab1/OpenGL01/main.cpp
+++ lab1/OpenGL01/main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     // necessary on macos monterey 12.6
-    // otherwise opengl 2.1 would be used :/
     QSurfaceFormat format;
     format.setDepthBufferSize(24);
     format.setStencilBufferSize(8);
